60895630c2fab486ae3f745d884b378859e205b9
Harsh2313 <kaushikh464@gmail.com>
2022-07-25 12:03:54 +0530

    started

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const route = require('./routes/routes.js')
+const { default: mongoose } = require('mongoose')
+const app = express()
+// const multer = require("multer")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use(multer().any())
+
+
+mongoose.connect("mongodb+srv://AshutoshGupta:ashutosh54264850@cluster0.ukus0.mongodb.net/AshutoshGupta", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route)
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port: ' + (process.env.PORT || 3000))
+}) 
